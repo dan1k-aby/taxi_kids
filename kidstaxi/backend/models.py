28a1c61812650b158160bde4d80829b91e645a4f
dan1k-aby <danik.aby@mail.ru>
2021-12-20 20:42:58 +0600

    timezone

--- kidstaxi/backend/models.py
+++ kidstaxi/backend/models.py
@@ -6,12 +6,13 @@ class Voditeli(models.Model):
     Sure_name = models.CharField("Фамилия", max_length=15)
     Date_of_birth = models.CharField("Дата рождения", max_length=10)
     number = models.CharField("Номер телефона", max_length=20)
-    Created = models.DateTimeField("Дата регистрации", auto_now=True)
+    Created = models.DateTimeField("Дата регистрации", auto_now_add=True)
     Auto = models.TextField("Автомобиль", max_length=20)
     Auto_number = models.CharField("госномер", max_length=10)
     
     class Meta:
         ordering = ("id",)
+
 
 class People(models.Model):
     Name = models.CharField("Имя", max_length=15)
